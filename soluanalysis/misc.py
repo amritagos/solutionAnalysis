d124ca659d8cccdf620aa33e992f7057f4d71ac1
Amrita Goswami <amrita16thaug646@gmail.com>
2024-09-16 13:23:16 +0000

    misc: Add exponential model

--- soluanalysis/misc.py
+++ soluanalysis/misc.py
@@ -20,6 +20,56 @@ def biexponential_model(t: float, A: float, tau1: float, tau2: float) -> float:
     """
     return A * np.exp(-t / tau1) + (1 - A) * np.exp(-t / tau2)
 
+def exponential_model(t: float, tau1: float) -> float:
+    """Fit data to an exponential decay
+
+    Args:
+        t (float): time (independent variable)
+        tau1 (float): time constant
+
+    Returns:
+        float: Result of the expression, used for fitting
+    """
+    return np.exp(-t / tau1) 
+
+def fit_exponential(
+    tau_timeseries: Union[List[float], npt.NDArray],
+    tcf_timeseries: Union[List[float], npt.NDArray],
+    initial_guess: Union[List[float], npt.NDArray] = [1],
+) -> Tuple[Tuple[float, float, float], npt.NDArray, npt.NDArray, float]:
+    """Fit 
+
+    Args:
+        tau_timeseries (Union[List[float], npt.NDArray]): Lag times for the TCF
+        tcf_timeseries (Union[List[float], npt.NDArray]): Time autocorrelation function values (this fit seems to work for the continuous bond definition).
+        Once a bond breaks, it is considered broken even if reformed.
+        initial_guess (Union[List[float], npt.NDArray], optional): Initial guess for parameters A, tau1 B, and tau2. Defaults to [0.5, 1, 2].
+
+    Returns:
+        Tuple[Tuple[float, float, float], npt.NDArray, npt.NDArray, float]: Returns a tuple of fitted parameters (A, tau1 and tau2), times, TCF values and the lifetime.
+    """
+
+    # Initial guess for the parameters [A, tau1, tau2]
+
+    # Increase the maxfev parameter to allow more iterations
+    params, params_covariance = curve_fit(
+        exponential_model,
+        tau_timeseries,
+        tcf_timeseries,
+        p0=initial_guess,
+        maxfev=10000,
+    )
+
+    fit_t = np.linspace(tau_timeseries[0], tau_timeseries[-1], 1000)
+    fit_ac = exponential_model(fit_t, *params)
+
+    # Extract the parameters
+    tau1 = params
+
+    # Compute the lifetime
+    lifetime = tau1 
+
+    return params, fit_t, fit_ac, lifetime
 
 def fit_biexponential(
     tau_timeseries: Union[List[float], npt.NDArray],
